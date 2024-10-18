82090542c9c4f1ea271c9d6f4b36501726e6bef1
unknown <ramovic225@gmail.com>
2024-10-18 19:21:33 +0200

    header added

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,13 @@ import './App.css'
 import Navbar from './components/Navbar'
 import GalleryPage from './pages/GalleryPage'
 import HomePage from './pages/HomePage'
+import Header from './components/Header'
 
 function App() {
 
   return (
     <div className="min-h-screen bg-gradient-to-r from-teal-900 via-teal-600 to-teal-900">
+      <Header />
       <Navbar />
       <Routes>
         <Route path="/" element={<HomePage />} />
