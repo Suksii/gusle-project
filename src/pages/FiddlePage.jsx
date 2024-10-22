6d6ff13b8bd63f550f4dcbadd7e5d49284c2cf1f
unknown <ramovic225@gmail.com>
2024-10-22 15:18:08 +0200

    animations added

--- src/pages/FiddlePage.jsx
+++ src/pages/FiddlePage.jsx
@@ -13,7 +13,12 @@ const FiddlePage = () => {
       >
         Gusle kao simbol bosnjacke nacije u crnoj gori
       </motion.h2>
-      <motion.p className="w-[90%] lg:w-1/3 mx-auto py-8 flex items-center text-teal-100 text-center text-xl leading-9">
+      <motion.p
+        className="w-[90%] lg:w-1/3 mx-auto py-8 flex items-center text-teal-100 text-center text-xl leading-9"
+        initial={{ opacity: 0, y: -50, scale: 0.9 }}
+        animate={{ opacity: 1, y: 0, scale: 1 }}
+        transition={{ duration: 0.8, ease: "easeOut" }}
+      >
         Gusle, tradicionalni balkanski gudački instrument, ima duboko ukorenjenu
         simboliku u narodnoj kulturi Crne Gore, kao i kod Bošnjaka. Iako je
         gusle često povezivana sa epovima i epskom tradicijom Srba i Crnogoraca,
@@ -21,7 +26,12 @@ const FiddlePage = () => {
         njihove kulturne baštine i identiteta.
       </motion.p>
       <div className="flex flex-col lg:flex-row p-4">
-        <motion.p className="w-[90%] lg:w-1/4 mx-auto flex items-center text-teal-100 text-center lg:text-start text-xl leading-9">
+        <motion.p
+          initial={{ opacity: 0, x: -50, scale: 0.9 }}
+          animate={{ opacity: 1, x: 0, scale: 1 }}
+          transition={{ duration: 0.8, ease: "easeOut" }}
+          className="w-[90%] lg:w-1/4 mx-auto flex items-center text-teal-100 text-center lg:text-start text-xl leading-9"
+        >
           Bošnjaci u Crnoj Gori koriste gusle da bi prenosili svoju istoriju,
           narodne pesme i priče koje često slave važne ličnosti i događaje iz
           njihove prošlosti. Gusle u bošnjačkom kontekstu često prenose poruke o
@@ -35,7 +45,12 @@ const FiddlePage = () => {
             className="w-[100px] mx-auto lg:w-[140px] lg:h-full"
           />
         </div>
-        <motion.p className="w-[90%] lg:w-1/4 mx-auto flex items-center text-teal-100 text-center lg:text-start text-xl leading-9">
+        <motion.p
+          className="w-[90%] lg:w-1/4 mx-auto flex items-center text-teal-100 text-center lg:text-start text-xl leading-9"
+          initial={{ opacity: 0, x: 50, scale: 0.9 }}
+          animate={{ opacity: 1, x: 0, scale: 1 }}
+          transition={{ duration: 0.8, ease: "easeOut" }}
+        >
           Za Bošnjake u Crnoj Gori, gusle su postale simbol nacionalnog
           identiteta jer su kroz ovaj instrument mogli da sačuvaju dio svoje
           kulturne i istorijske baštine, posebno tokom perioda kada su bili
@@ -45,7 +60,12 @@ const FiddlePage = () => {
           tradicijom.
         </motion.p>
       </div>
-      <motion.p className="w-[90%] lg:w-1/3 mx-auto py-8 flex items-center text-teal-100 text-center text-xl leading-9">
+      <motion.p
+        className="w-[90%] lg:w-1/3 mx-auto py-8 flex items-center text-teal-100 text-center text-xl leading-9"
+        initial={{ opacity: 0, y: 50, scale: 0.9 }}
+        animate={{ opacity: 1, y: 0, scale: 1 }}
+        transition={{ duration: 0.8, ease: "easeOut" }}
+      >
         Iako je gusle uopšteno više poznata kao simbol crnogorske i srpske
         tradicije, u bošnjačkom narodu Crne Gore ona je alat kojim se čuva i
         izražava kulturni i nacionalni identitet, pokazujući njenu univerzalnost
